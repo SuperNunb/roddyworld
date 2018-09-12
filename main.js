a4aab90bc90581ef4de40735147a395a464b6630
SuperNunb <33640719+SuperNunb@users.noreply.github.com>
2018-09-12 07:45:22 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-window.open("index.html", "_self");
+//window.open("index.html", "_self");
+const http = require('http');
+const fs = require('fs');
+const html = fs.readFileSync('index.html');
+
+http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(html);
+}).listen(process.env.PORT || 5000);
